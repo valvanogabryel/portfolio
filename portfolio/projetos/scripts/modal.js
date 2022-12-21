1cb594e58761e4420659653cfa98482c8d293f34
valvanogabryel <valvanogabryel@gmail.com>
2022-12-21 12:47:07 -0300

    ;

--- portfolio/projetos/scripts/modal.js
+++ portfolio/projetos/scripts/modal.js
@@ -1,7 +1,44 @@
+const modal = document.getElementById('modal-projects');
 const btns = document.querySelectorAll('button.button__activator');
+const modalTechs = document.querySelector('p#modal-text');
+const siteBtn = document.querySelector('#btn-site');
+const repositoryBtn = document.querySelector('#btn-repository');
 const projects = {
     project1: {
-        techs: HTML & CSS
+        name: 'projeto-selfcare',
+        techs: 'HTML & CSS',
+        link: 'https://valvanogabryel.github.io/selfcare/index.html',
+        repository: 'https://github.com/valvanogabryel/selfcare'
+    },
+    project2: {
+        name: 'projeto-calculadora-de-gorjeta',
+        techs: 'HTML, CSS & JS',
+        link: 'https://valvanogabryel.github.io/projetos/projetos-treinamento/funcionalidades/tip-calculator-app/index.html',
+        repository: 'https://github.com/valvanogabryel/projetos/tree/main/projetos-treinamento/funcionalidades/tip-calculator-app'
+    },
+    project3: {
+        name: 'tic-tac-toe',
+        techs: 'HTML, CSS & JS',
+        link: 'https://valvanogabryel.github.io/selfcare/index.html',
+        repository: 'https://github.com/valvanogabryel/selfcare'
+    },
+    project4: {
+        name: 'projeto-calculadora-de-gorjeta',
+        techs: 'HTML, CSS & JS',
+        link: 'https://valvanogabryel.github.io/projetos/projetos-treinamento/funcionalidades/tip-calculator-app/index.html',
+        repository: 'https://github.com/valvanogabryel/projetos/tree/main/projetos-treinamento/funcionalidades/tip-calculator-app'
+    },
+    project5: {
+        name: 'projeto-selfcare',
+        techs: 'HTML & CSS',
+        link: 'https://valvanogabryel.github.io/selfcare/index.html',
+        repository: 'https://github.com/valvanogabryel/selfcare'
+    },
+    project6: {
+        name: 'projeto-calculadora-de-gorjeta',
+        techs: 'HTML, CSS & JS',
+        link: 'https://valvanogabryel.github.io/projetos/projetos-treinamento/funcionalidades/tip-calculator-app/index.html',
+        repository: 'https://github.com/valvanogabryel/projetos/tree/main/projetos-treinamento/funcionalidades/tip-calculator-app'
     }
 
 }
@@ -12,15 +49,35 @@ btns.forEach(btn => {
 
 
 function handleModal(event) {
-    console.log(event);
+    switch (event.target.alt) {
+        case projects.project1.name:
+            modalTechs.innerHTML = projects.project1.techs
+            siteBtn.href = projects.project1.link
+            repositoryBtn.href = projects.project1.repository
+            break
+
+        case projects.project2.name:
+            modalTechs.innerHTML = projects.project2.techs
+            siteBtn.href = projects.project2.link
+            repositoryBtn.href = projects.project2.repository
+            break
+    }
 
 
 
 
+
+
+
+    startModal();
 }
 
-function startModal(modalId) {
-    const modal = document.getElementById(modalId);
+function startModal() {
+    // const modal = document.getElementById(modalId);
     modal.classList.add('show');
+
+    modal.addEventListener('click', event => {
+        if (event.target.id === 'modal-projects') modal.classList.remove('show');
+    })
 }
 
